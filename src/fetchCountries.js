40704891514632c15921b61600d3dc35c7dc994e
Yevhenii Yakovenko <yakovenkob@gmail.com>
2022-10-18 22:39:48 +0300

    first step

---
+++ src/fetchCountries.js
@@ -0,0 +1,5 @@
+export function fetchCountries(name) {
+    return fetch(`https://restcountries.com/v3.1/name/${name}?fields=name,name.official,capital,population,flags,languages`).then(response => {
+        return response.json();
+    });
+}
--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 import './css/styles.css';
+import { fetchCountries } from './fetchCountries';
+import { debounce } from 'lodash';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
 const DEBOUNCE_DELAY = 300;
+
+
+const refs = {
+    input: document.querySelector('#search-box'),
+    country: document.querySelector('.country-list'),
+    countryInfo: document.querySelector('country-info'),
+}
+
+refs.input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
+
+function onInput(e) {
+    const inputValue = e.target.value.trim();
+    if (!inputValue) {
+        refs.country.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
+        return;
+    }
+    fetchCountries(inputValue).then(countries => {
+        if (countries.length > 10) {
+            return Notify.info("Too many matches found. Please enter a more specific name.")
+        }
+    });
+
+} 
+
+
+
+
+
+
+
+
